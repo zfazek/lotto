7a97d664dd1e4244968868088b5f683986d306dc
zfazek <ongroa@gmail.com>
2022-01-17 13:59:59 +0100

    add unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,11 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn generate_numbers(n: usize, limit: usize, name: &str) -> String {
+    let numbers = generate_random_numbers(n, limit);
+    format_set(&numbers, name)
+}
+
+fn generate_random_numbers(n: usize, limit: usize) -> BTreeSet<usize> {
     let mut numbers = BTreeSet::new();
     let mut buf = [0u8; 1];
     while numbers.len() < n {
@@ -16,6 +21,10 @@ pub fn generate_numbers(n: usize, limit: usize, name: &str) -> String {
         let secret_number = (buf[0] % limit as u8 + 1) as usize;
         numbers.insert(secret_number);
     }
+    numbers
+}
+
+fn format_set(numbers: &BTreeSet<usize>, name: &str) -> String {
     let mut str = String::from(name.to_string() + "[");
     for (i, number) in numbers.iter().enumerate() {
         if number < &10 {
@@ -28,4 +37,35 @@ pub fn generate_numbers(n: usize, limit: usize, name: &str) -> String {
     }
     str += "]";
     str
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_random_numbers() {
+        for _ in 0..1000 {
+            let n = 3;
+            let limit = 10;
+            let numbers = generate_random_numbers(n, limit);
+            assert_eq!(numbers.len(), n);
+            let v: Vec<usize> = numbers.into_iter().collect();
+            for idx in 0..v.len() - 1 {
+                assert!(v[idx] < v[idx + 1]);
+                assert!(v[idx] >= 1);
+                assert!(v[idx] <= limit);
+            }
+        }
+    }
+
+    #[test]
+    fn test_format_set() {
+        let mut numbers = BTreeSet::new();
+        numbers.insert(10);
+        numbers.insert(5);
+        numbers.insert(8);
+        let result = format_set(&numbers, "foo: ");
+        assert_eq!("foo: [ 5,  8, 10]", result);
+    }
 }
