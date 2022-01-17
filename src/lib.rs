319c41e95f4a925430fb84bdc4cb8ae4d3738afa
zfazek <ongroa@gmail.com>
2022-01-17 11:26:18 +0100

    use BTreeSet instead of HashSet for numbers

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use wasm_bindgen::prelude::*;
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,22 +9,17 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn generate_numbers(n: usize, limit: usize, name: &str) -> String {
-    let mut numbers = HashSet::new();
+    let mut numbers = BTreeSet::new();
     let mut buf = [0u8; 1];
     while numbers.len() < n {
         getrandom::getrandom(&mut buf).unwrap();
         let secret_number = (buf[0] % limit as u8 + 1) as usize;
         numbers.insert(secret_number);
     }
-    let mut ret = Vec::new();
-    for n in numbers {
-        ret.push(n);
-    }
-    ret.sort();
     let mut str = String::from(name.to_string() + "[");
-    for (i, n) in ret.iter().enumerate() {
+    for (i, n) in numbers.iter().enumerate() {
         str += &n.to_string();
-        if i < ret.len() - 1 {
+        if i < numbers.len() - 1 {
             str += ", ";
         }
     }
